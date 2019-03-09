21ff92b95d43c3411a577f858c34bdab20fc26f1
Luisa Scavo <luisascavo@gmail.com>
2019-03-09 16:55:17 -0500

    fix typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', deruation: '2:30' },
+    { title: 'Macarena', duration: '2:30' },
     { title: 'All Star', duration: '3:15' },
     { title: "I Want it That Way", duration: '3:30'}
   ];
