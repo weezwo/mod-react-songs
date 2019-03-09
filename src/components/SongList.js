4f0895b973f082e377fe8f7a77d33700a8de255f
Luisa Scavo <luisascavo@gmail.com>
2019-03-09 16:37:50 -0500

    wire up song selection

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
 
@@ -8,7 +9,10 @@ class SongList extends React.Component {
       return(
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button 
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -26,4 +30,6 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
+
+// second argument to connect refers to actions: these get "wrapped" in state.dispatch
